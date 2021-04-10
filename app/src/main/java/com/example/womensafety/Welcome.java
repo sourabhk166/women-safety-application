45ab7b9663a8c532bc943968c66af8323641c306
sourav <sourabhk166@gmail.com>
2021-04-10 08:54:06 +0530

    Initial commit.

---
+++ app/src/main/java/com/example/womensafety/Welcome.java
@@ -0,0 +1,37 @@
+package com.example.womensafety;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Welcome extends AppCompatActivity {
+
+    Timer timer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+
+        setContentView(R.layout.activity_welcome);
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Welcome.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },4000);
+    }
+}
